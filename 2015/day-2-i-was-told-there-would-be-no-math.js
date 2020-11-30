58ac64c812abeebd222d4b88984d13a6b8b24cf4
Patrick Walsh <patrick@fastforward.sh>
2020-11-30 14:57:27 -0500

    AoC 2015 Day 2

--- 2015/day-2-i-was-told-there-would-be-no-math.js
+++ 2015/day-2-i-was-told-there-would-be-no-math.js
@@ -1,6 +1,6 @@
 const data = require("./data/day-2.json")
 
-const parseLine = line => {
+const parseLine = (line) => {
   return {
     l: line.split("x")[0],
     w: line.split("x")[1],
@@ -9,30 +9,45 @@ const parseLine = line => {
 }
 
 const transformValues = (obj, cb) => {
-  Object.keys(obj).forEach(key => {
+  Object.keys(obj).forEach((key) => {
     obj[key] = cb(obj[key])
   })
   return obj
 }
 
-const calculateSides = parsedLine => {
+const calculateSidesAreas = (parsedLine) => {
   return [
     parsedLine.l * parsedLine.w,
     parsedLine.w * parsedLine.h,
-    parsedLine.h * parsedLine.l
+    parsedLine.h * parsedLine.l,
   ]
 }
 
-const findSmallestSide = sidesArr => {
-  return sidesArr.reduce((out, curr) => out < curr ? out : curr)
+const findSmallestSide = (sidesAreasArr) => {
+  return sidesAreasArr.reduce((out, curr) => (out < curr ? out : curr))
 }
 
-const wrappingPaperPerBox = line => {
+const wrappingPaperPerBox = (line) => {
   let boxDimensions = transformValues(parseLine(line), parseInt)
-  let boxSideAreas = calculateSides(boxDimensions)
+  let boxSideAreas = calculateSidesAreas(boxDimensions)
   let smallestSide = findSmallestSide(boxSideAreas)
-  return boxSideAreas.reduce((out, curr) => out + (2*curr), 0) + smallestSide
+  return boxSideAreas.reduce((out, curr) => out + 2 * curr, 0) + smallestSide
 }
 
 console.log(data.reduce((out, curr) => out + wrappingPaperPerBox(curr), 0))
 
+const shortestPerimeter = (parsedLine) => {
+  const sizes = Object.values(parsedLine)
+  return 2 * (sizes.reduce((out, curr) => out + curr) - Math.max(...sizes))
+}
+
+const packageVolume = (parsedLine) => {
+  return Object.values(parsedLine).reduce((out, curr) => out * curr)
+}
+
+const ribbonPerBox = (line) => {
+  const parsedLine = transformValues(parseLine(line), parseInt)
+  return shortestPerimeter(parsedLine) + packageVolume(parsedLine)
+}
+
+console.log(data.reduce((out, curr) => out + ribbonPerBox(curr), 0))
