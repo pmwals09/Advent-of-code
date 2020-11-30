5f67766860a2b4b97cb5128fe6aec5b459afbbbc
Patrick Walsh <patrick@fastforward.sh>
2020-11-30 16:50:35 -0500

    AOC 2015 Day 3 WIP

--- 2015/day-3-perfectly-spherical-houses-in-a-vacuum.js
+++ 2015/day-3-perfectly-spherical-houses-in-a-vacuum.js
@@ -1,61 +1,94 @@
 const data = require("./data/day-3.json")
 
-const chooseMove = move => {
+const chooseMove = (move, historyArr, currentPosition) => {
   switch (move) {
     case "^":
-      moveUp(historyArr, currentPosition)
-      break;
+      return moveUp(historyArr, currentPosition)
     case ">":
-      moveRight(historyArr, currentPosition)
-      break;
+      return moveRight(historyArr, currentPosition)
     case "v":
-      moveDown(historyArr, currentPosition)
-      break;
+      return moveDown(historyArr, currentPosition)
     case "<":
-      moveLeft(historyArr, currentPosition)
-      break;
+      return moveLeft(historyArr, currentPosition)
     default:
-      return "Somethign went wrong..."
+      return "Something went wrong in chooseMove..."
   }
 }
 
-const moveUp = (historyArr = [1], currentPosition) => {
-  if(currentPosition[0] === 0){
-    const newRow = historyArr[0].map(ea => ea = 0)
-    historyArr.push(newRow)
+const incrementCurrentPosition = (historyArr, currentPosition) => {
+  historyArr[currentPosition[0]][currentPosition[1]]++
+}
+
+const moveUp = (historyArr = [[1]], currentPosition = [0,0]) => {
+  if (currentPosition[0] === 0) {
+    addRow(historyArr, "top")
   } else {
-    // move up a row
+    currentPosition[0]--
   }
-  // increment where you land
-  historyArr[currentPosition[0]][currentPosition[1]] = 1
+  incrementCurrentPosition(historyArr, currentPosition)
+  return historyArr
 }
 
-const moveRight = (historyArr, currentPosition) => {
-  // if you're at the end of the row, add a column
-  // else move up a column
-  // increment where you land
+const moveRight = (historyArr = [[1]], currentPosition = [0,0]) => {
+  if(currentPosition[1] === historyArr[0].length - 1){
+    addCol(historyArr, 'right')
+  }
+  currentPosition[1]++
+  incrementCurrentPosition(historyArr, currentPosition)
+  return historyArr
 }
 
-const moveDown = (historyArr, currentPosition) => {
-  // if you're in the last row, add a row
-  // else move down a row
-  // increment where you land
+const moveDown = (historyArr = [[1]], currentPosition = [0,0]) => {
+  if (currentPosition[0] === historyArr.length - 1) {
+    addRow(historyArr, "bottom")
+  }
+  currentPosition[0]++
+  incrementCurrentPosition(historyArr, currentPosition)
+  return historyArr
 }
 
-const moveLeft = (historyArr, currentPosition) => {
-  // if you're in the first col, add a col
-  // else move down a col
-  // increment where you land
+const moveLeft = (historyArr = [[1]], currentPosition = [0,0]) => {
+  if(currentPosition[1] === 0) {
+    addCol(historyArr, 'left')
+  }
+  incrementCurrentPosition(historyArr, currentPosition)
+  return historyArr
 }
 
-const addRow = () => {
-
+const addRow = (historyArr, whereToAdd) => {
+  const newRow = historyArr[0].map((ea) => (ea = 0))
+  switch (whereToAdd) {
+    case "top":
+      historyArr.unshift(newRow)
+      break
+    case "bottom":
+      historyArr.push(newRow)
+      break
+    default:
+      return "something went wrong..."
+  }
 }
 
-const addCol = () => {
-
+const addCol = (historyArr, whereToAdd) => {
+  switch (whereToAdd) {
+    case "left":
+      historyArr.map((ea) => ea.unshift(0))
+      break
+    case "right":
+      historyArr.map((ea) => ea.push(0))
+      break
+    default:
+      return "something went wrong..."
+  }
 }
 
-const incrementPosition = () => {
-  
+const followDirections = directionString => {
+  let historyArr = [[1]]
+  currentPosition = [0,0]
+  directionString.split('').forEach(move => {
+    historyArr = chooseMove(move, historyArr, currentPosition)
+  });
+  return historyArr
 }
+
+console.log(followDirections(data).map(row => row.filter(house => house !== 0)).reduce((out, curr) => out + curr.length, 0))
