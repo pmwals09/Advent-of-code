9150cae5497b9f5d16848a660a649f7b332ccdc0
Pat Walsh <pmwals09@gmail.com>
2020-12-04 20:05:38 -0500

    AOC 2020 Day 2

--- 2020/day-2-password-philosophy.js
+++ 2020/day-2-password-philosophy.js
@@ -1,49 +1,52 @@
-const data = require("./data/day-2.json")
+const data = require("./data/day-2.json");
 
-const parseInput = line => {
-  let minOccur = line.split("-")[0]
-  let remainingPieces = line.split("-")[1].split(" ")
-  let [maxOccur, targetLetter, password] = remainingPieces
+const parseInput = (line) => {
+  let minOccur = line.split("-")[0];
+  let remainingPieces = line.split("-")[1].split(" ");
+  let [maxOccur, targetLetter, password] = remainingPieces;
   return {
     minOccur: parseInt(minOccur),
     maxOccur: parseInt(maxOccur),
     targetLetter: targetLetter[0],
-    password
-  }
-}
+    password,
+  };
+};
 
-const parsePassword = password => {
+const parsePassword = (password) => {
   return password.split("").reduce((out, curr) => {
-    if(out[curr]) return {...out, [curr]: out[curr] + 1}
-    else return {...out, [curr]: 1}
-  }, {})
-}
+    if (out[curr]) return { ...out, [curr]: out[curr] + 1 };
+    else return { ...out, [curr]: 1 };
+  }, {});
+};
 
-const passwordOccurrenceTest = line => {
-  const parsedInput = parseInput(line)
-  let {minOccur, maxOccur, targetLetter, password} = parsedInput
-  const targetLetterFrequency = parsePassword(password)[targetLetter]
-  return targetLetterFrequency <= maxOccur && targetLetterFrequency >= minOccur
-}
+const passwordOccurrenceTest = (line) => {
+  const parsedInput = parseInput(line);
+  let { minOccur, maxOccur, targetLetter, password } = parsedInput;
+  const targetLetterFrequency = parsePassword(password)[targetLetter];
+  return targetLetterFrequency <= maxOccur && targetLetterFrequency >= minOccur;
+};
 
-console.log(data.reduce((out, curr) => {
-  return passwordOccurrenceTest(curr) ? out + 1 : out
-}, 0))
+console.log(
+  data.reduce((out, curr) => {
+    return passwordOccurrenceTest(curr) ? out + 1 : out;
+  }, 0)
+);
 
-const passwordPositionTest = line => {
-    const parsedInput = parseInput(line);
-    let { minOccur, maxOccur, targetLetter, password } = parsedInput;
-    return (
-      (password[minOccur - 1] === targetLetter ||
-        password[maxOccur - 1] === targetLetter) &&
-      !(
-        password[minOccur - 1] === targetLetter &&
-        password[maxOccur - 1] === targetLetter
-      )
-    );
+const passwordPositionTest = (line) => {
+  const parsedInput = parseInput(line);
+  let { minOccur, maxOccur, targetLetter, password } = parsedInput;
+  return (
+    (password[minOccur - 1] === targetLetter ||
+      password[maxOccur - 1] === targetLetter) &&
+    !(
+      password[minOccur - 1] === targetLetter &&
+      password[maxOccur - 1] === targetLetter
+    )
+  );
+};
 
-}
-
-console.log(data.reduce((out, curr) => {
-  return passwordPositionTest(curr) ? out + 1 : out
-}, 0))
+console.log(
+  data.reduce((out, curr) => {
+    return passwordPositionTest(curr) ? out + 1 : out;
+  }, 0)
+);
